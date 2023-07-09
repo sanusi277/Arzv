be9a270cba050943cf1d609a32eb67cfadb2eb8f
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-07-09 12:33:22 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -514,10 +514,10 @@ echo  "------------------------------------------------------------"
 echo  ""
 echo  "   >>> Service & Port"  | tee -a log-install.txt
 echo  "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo  "   - SSH Websocket           : 80" | tee -a log-install.txt
+echo  "   - SSH Websocket           : 80,2082" | tee -a log-install.txt
 echo  "   - SSH SSL Websocket       : 443" | tee -a log-install.txt
-echo  "   - Stunnel5                : 447, 777" | tee -a log-install.txt
-echo  "   - Dropbear                : 109, 143" | tee -a log-install.txt
+echo  "   - Stunnel5                : 447,777" | tee -a log-install.txt
+echo  "   - Dropbear                : 109,143" | tee -a log-install.txt
 echo  "   - Badvpn                  : 7100-7300" | tee -a log-install.txt
 echo  "   - Nginx                   : 81" | tee -a log-install.txt
 echo  "   - XRAY  Vmess TLS         : 443" | tee -a log-install.txt
