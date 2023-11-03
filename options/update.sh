61d88110a26f3e80152d75593c1299d840cdb006
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-11-03 20:20:25 +0700

    Update update.sh

--- options/update.sh
+++ options/update.sh
@@ -1,3 +1,53 @@
+#!/bin/bash
+# Source Script Arz
+
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+
 #wget https://github.com/${GitUser}/
 GitUser="arzvpn"
 if [ "${EUID}" -ne 0 ]; then
@@ -37,24 +87,21 @@ sts="${Error}"
 fi
 clear
 echo ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$back_text                 \e[30m[\e[$box CHECK NEW UPDATE\e[30m ]                   \e[m"
-echo -e "   \e[$line--------------------------------------------------------\e[m"
 echo -e "   ┌─────────────────────────────────────────────────────┐" | lolcat
 echo -e "   │                      MENU UPDATE                    │" | lolcat
 echo -e "   └─────────────────────────────────────────────────────┘" | lolcat
-echo -e "   \e[$below VVERSION NOW >> $Info1"
-echo -e "   \e[$below SSTATUS UPDATE >> $sts"
+echo -e "   VERSION NOW >> $Info1"
+echo -e "   STATUS UPDATE >> $sts"
 echo -e ""
 echo -e "   ======================================= " | lolcat   
 echo ""
 echo -e ""
-echo -e "     \e[$number [1]\e[m \e[$below CCheck Script Update Now\e[m"
-echo -e "     \e[$number [x]\e[m \e[$below BBack To Menu\e[m"
+echo -e "     [1] Check Script Update Now"
+echo -e "     [x] Back To Menu"
 echo -e ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
+echo -e "   \033[0;34m--------------------------------------------------------\033[0m"
 echo -e "\e[$line"
-read -p "PPlease Choose 1 or x : " option2
+read -rp "Please Choose 1 or x : " option2
 case $option2 in
 1)
 version=$(cat /home/ver)
