48b4430aba58711d39fc0b7d3079fbe8f18f5cfc
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-11-04 22:33:33 +0700

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -49,7 +49,6 @@ echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring admin data..."
 sleep 1
 cp -r /root/backup/scrz-prem /var/lib/ &> /dev/null
 cp -r /root/backup/.acme.sh /root/ &> /dev/null
-cp -r /root/backup/arzvpn /etc/ &> /dev/null
 cp -r /root/backup/xray /etc/ &> /dev/null
 cp -r /root/backup/xray /usr/local/etc/ &> /dev/null
 cp -r /root/backup/xray /usr/bin/ &> /dev/null
