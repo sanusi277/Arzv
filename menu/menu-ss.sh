e017a2cd9f367d1219c7e2aee82d9151f94a6bb1
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-11-05 15:24:55 +0700

    Update menu-ss.sh

--- menu/menu-ss.sh
+++ menu/menu-ss.sh
@@ -251,7 +251,7 @@ lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -
 echo -e "user :${GREEN} ${akun} ${NC}
 ${RED}Online Jam ${NC}: ${lastlogin} wib";
 echo -e "$jum2";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 fi
 rm -rf /tmp/ipxray.txt
 done
